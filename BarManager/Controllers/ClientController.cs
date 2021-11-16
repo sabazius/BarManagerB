d49ba43f60217b7af9abe0916fae30fc64cd2661
HrStanchev <hristoo9363@gmail.com>
2021-11-16 18:48:09 +0200

    Create Client Model

---
+++ BarManager/Controllers/ClientController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BarManager.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarManager.Host.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientController : ControllerBase
+    {
+
+        public ClientController()
+        {
+
+        }
+
+        [HttpGet]
+        public Client Get()
+        {
+            return new Client()
+            {
+                Id = 1,
+                Name = "TestClient",
+                MoneySpend = DateTime.Today,
+                Discount = 4
+            };
+        }
+
+
+    }
+}
