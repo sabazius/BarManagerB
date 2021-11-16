e7b70e489d207fde6fc05ad296bcbe15a162c7a2
AnitaDoykova <anitadoykova@gmail.com>
2021-11-16 18:40:17 +0200

    Create Products Model

---
+++ BarManager.Models/DTO/Products.cs
@@ -0,0 +1,12 @@
+﻿namespace BarManager.Models.DTO
+{
+    public class Products
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+    }
+}
