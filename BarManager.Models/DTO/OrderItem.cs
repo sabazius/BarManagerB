450d464b704e57a5a55ffcbbc7409c1feb7e280c
scorpi078 <provan25@gmail.com>
2021-11-16 18:46:17 +0200

    Done

---
+++ BarManager.Models/DTO/OrderItem.cs
@@ -0,0 +1,21 @@
+﻿using BarManager.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BarManager.Models.DTO
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public OrderType Type { get; set; }
+
+        public List<Tag> Tags { get; set; }
+       
+    }
+}
