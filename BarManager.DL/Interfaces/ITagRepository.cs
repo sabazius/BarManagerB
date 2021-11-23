a830501cc0ce0adfdef5f3b4c800bb28057f2d9b
b.serafimov <b.b.serafimov@gmail.com>
2021-11-23 17:54:38 +0200

    Create DL layer and Tag repo and controller

---
+++ BarManager.DL/Interfaces/ITagRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using BarManager.Models.DTO;
+
+namespace BarManager.DL.Interfaces
+{
+    public interface ITagRepository
+    {
+        Tag Create(Tag tag);
+
+        Tag Update(Tag tag);
+
+        Tag Delete(int id);
+
+        Tag GetById(int id);
+
+        IEnumerable<Tag> GetAll();
+    }
+}
---
+++ BarManager.DL/Repositories/InMemoryRepos/TagInMemoryRepository.cs
@@ -0,0 +1,52 @@
+﻿using BarManager.DL.Interfaces;
+using BarManager.Models.DTO;
+using System.Collections.Generic;
+using System.Linq;
+using BarManager.DL.InMemoryDb;
+
+namespace BarManager.DL.Repositories.InMemoryRepos
+{
+    public class TagInMemoryRepository : ITagRepository
+    {
+
+        public TagInMemoryRepository()
+        {
+            
+        }
+
+        public Tag Create(Tag tag)
+        {
+            TagInMemoryCollection.TagDb.Add(tag);
+
+            return tag;
+        }
+
+        public Tag Delete(int id)
+        {
+            var tag = TagInMemoryCollection.TagDb.FirstOrDefault(tag => tag.Id == id);
+
+            TagInMemoryCollection.TagDb.Remove(tag);
+
+            return tag;
+        }
+
+        public IEnumerable<Tag> GetAll()
+        {
+            return TagInMemoryCollection.TagDb;
+        }
+
+        public Tag GetById(int id)
+        {
+            return TagInMemoryCollection.TagDb.FirstOrDefault(x => x.Id == id);
+        }
+
+        public Tag Update(Tag tag)
+        {
+            var result = TagInMemoryCollection.TagDb.FirstOrDefault(x => x.Id == tag.Id);
+
+            result.Name = tag.Name;
+
+            return result;
+        }
+    }
+}
