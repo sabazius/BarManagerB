c805e70177735d53a53e6457baa2c0f7841b4d40
feelssogood <y#8GZ0e`+yHQuAs&aw4i>
2021-12-06 00:04:36 +0200

    asd

---
+++ BarManager.DL/InMemoryDb/FurnituresInMemoryCollection.cs
@@ -0,0 +1,54 @@
+﻿using BarManager.Models.DTO;
+using BarManager.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BarManager.DL.InMemoryDb
+{
+    class FurnituresInMemoryCollection
+    {
+        public static List<Furnitures> FurnituresDb = new List<Furnitures>()
+        {
+            new Furnitures()
+            {
+                Id = 1,
+                Type = FurnituresType.Chair,
+                Location = new List<Location>()
+                {
+                    new Location()
+                    {
+                        Id = 1,
+                        Name = "Warehouse"
+                    }
+                }
+            },
+            new Furnitures()
+            {
+                Id = 2,
+                Type = FurnituresType.Table,
+                Location = new List<Location>()
+                {
+                    new Location()
+                    {
+                        Id = 2,
+                        Name = "Bar"
+                    }
+                }
+            },
+            new Furnitures()
+            {
+                Id = 3,
+                Type = FurnituresType.Umbrella,
+                Location = new List<Location>()
+                {
+                    new Location()
+                    {
+                        Id = 3,
+                        Name = "Entrance"
+                    }
+                }
+            },
+        };
+    }
+}
--- BarManager/Controllers/FurnituresController.cs
+++ BarManager/Controllers/FurnituresController.cs
@@ -2,6 +2,7 @@ using System.Runtime.InteropServices;
 using BarManager.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using BarManager.DL.Interfaces;
 
 namespace BarManager.Host.Controllers
 {
@@ -9,25 +10,65 @@ namespace BarManager.Host.Controllers
     [Route("[controller]")]
     public class FurnituresController : ControllerBase
     {
+        private readonly IFurnituresRepository _furnituresRepository;
 
-        public FurnituresController()
+        public FurnituresController(IFurnituresRepository furnituresRepository)
         {
+            _furnituresRepository = furnituresRepository;
+        }
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            var result = _furnituresRepository.GetAll();
 
+            return Ok(result);
+        }
+        [HttpGet("GetById")]
+        public IActionResult GetById(int id)
+        {
+            if (id <= 0) return BadRequest();
+
+            var result = _furnituresRepository.GetById(id);
+
+            if (result == null) return NotFound(id);
+
+            return Ok(result);
         }
 
-        [HttpGet]
-        public Furnitures Get()
+        [HttpPost("Create")]
+        public IActionResult CreateTag([FromBody] Furnitures furnitures)
         {
-            var location = new Location() { Id = 3, Name = "Warehouse" };
+            if (furnitures == null) return BadRequest();
 
-            return new Furnitures()
-            {
-                Id = 1,
-                Type = Models.Enums.FurnituresType.Chair,
-                Location = location
-            };
+            var result = _furnituresRepository.Create(furnitures);
+
+            return Ok(furnitures);
         }
 
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            if (id <= 0) return BadRequest(id);
 
+            var result = _furnituresRepository.Delete(id);
+
+            if (result == null) return NotFound(id);
+
+            return Ok(result);
+        }
+        [HttpPost("Update")]
+        public IActionResult Update([FromBody] Furnitures furnitures) { 
+         
+        if (furnitures == null) return BadRequest();
+
+        var searchTag = _furnituresRepository.GetById(furnitures.Id);
+
+            if (searchTag == null) return NotFound(furnitures.Id);
+
+        var result = _furnituresRepository.Update(furnitures);
+
+            return Ok(result);
     }
 }
+}
+
